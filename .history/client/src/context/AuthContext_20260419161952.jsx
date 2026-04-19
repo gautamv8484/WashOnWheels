673acf52d666d4fa2acaac6e43e67e640bf0819b
Gautam Vyas <gautamv8484@gmail.com>
2026-04-19 16:21:48 +0530

    updated version

--- .history/client/src/context/AuthContext_20260419161952.jsx
+++ .history/client/src/context/AuthContext_20260419161952.jsx
@@ -4,8 +4,6 @@ import axios from 'axios'
 const AuthContext = createContext(null)
 
 const API_URL = import.meta.env.VITE_API_URL + '/api'
-
-
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
--- .history/client/src/context/AuthContext_20260419161953.jsx
+++ .history/client/src/context/AuthContext_20260419161953.jsx
@@ -5,7 +5,6 @@ const AuthContext = createContext(null)
 
 const API_URL = import.meta.env.VITE_API_URL + '/api'
 
-
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
